a25c40be6c6cfa48c6b0c77b3d104149fb33b00c
AssaadHalabi <asaadalhalabi@gmail.com>
2024-08-04 01:42:38 +0300

    feat: update useAuth hook to return authenticated user email

--- Frontend/hooks/useAuth.ts
+++ Frontend/hooks/useAuth.ts
@@ -5,19 +5,24 @@ import api from '@/lib/axios';
 export const useAuth = () => {
   const [loading, setLoading] = useState(true);
   const [authenticated, setAuthenticated] = useState(false);
+  const [email, setEmail] = useState("");
   const router = useRouter();
 
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const response = await api.get('/api/check-auth');
+        const { data, status } = await api.get('/api/check-auth');
         setAuthenticated(true);
-        alert(JSON.stringify(response))
-      } catch (error) {
+        if (status === 200) setEmail(data.email)
+      } catch (error: any) {
         setAuthenticated(false);
-        console.log(error);
-        
-        // router.push('/login');  // Redirect to login page if not authenticated
+        if (error.cause === 401) {
+          if (typeof window !== 'undefined') {
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('refreshToken');
+          }
+          router.push("/authentication")
+        }
       } finally {
         setLoading(false);
       }
@@ -26,7 +31,7 @@ export const useAuth = () => {
     checkAuth();
   }, [router]);
 
-  return { loading, authenticated };
+  return { loading, authenticated, email };
 };
 
 export default useAuth;
