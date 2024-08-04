b2ad96f7c9e62a9fa76e6d326352fbb33139baf3
AssaadHalabi <asaadalhalabi@gmail.com>
2024-08-05 01:32:58 +0300

    feat: add dashboard test component

---
+++ Frontend/app/dashboard/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import Loader from '@/components/Loader';
+import useAuth from '@/hooks/useAuth';
+import React from 'react';
+
+const Dashboard = () => {
+  const { loading, authenticated, email } = useAuth();
+
+  if (loading) {
+    return <Loader />;
+  }
+
+
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <h1 className='m-14'>{authenticated.toString()} motherfucker</h1>
+      Hello {email} nigger
+    </div>
+  );
+};
+
+export default Dashboard;
