510f28581e6079d8cf5a768a24da1ae1e8538710
AssaadHalabi <asaadalhalabi@gmail.com>
2024-08-05 10:33:57 +0300

    feat: frontend wrap pricing component with StripeProvider

--- Frontend/app/pricing/page.tsx
+++ Frontend/app/pricing/page.tsx
@@ -1,3 +1,14 @@
+"use client"
 
+import StripeProvider from '@/components/StripeProvider';
+import Pricing from '@/components/pricing/pricing';
 
-export { default } from "@/components/pricing/pricing";
+const PricingPage = () => {
+  return (
+    <StripeProvider>
+      <Pricing />
+    </StripeProvider>
+  );
+}
+
+export default PricingPage;
